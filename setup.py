e8b9f01c0f4024f147a520bd7a705561b9fd3322
simeonreusch <simeon.reusch@desy.de>
2022-07-20 15:33:25 +0200

    bugfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nuztf",
+    name="planobs",
     version="0.4.3",
     author="Simeon Reusch",
     author_email="simeon.reusch@desy.de",
