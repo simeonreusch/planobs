2b51d94ff2322f76fde8732792ccb3870d57ee24
simeonreusch <simeon.reusch@desy.de>
2022-09-19 07:56:10 +0200

    icecube plan changed

--- icecube_plan.py
+++ icecube_plan.py
@@ -10,21 +10,27 @@ logging.basicConfig()
 logging.getLogger("planobs.plan").setLevel(logging.INFO)
 logging.getLogger("planobs.gcn_parser").setLevel(logging.INFO)
 
-name = "IC220624A"  # Name of the alert object
+name = "IC220907A"  # Name of the alert object
 # name = "IC201007A"
-date = "2022-06-24"  # This is optional, defaults to today
+date = "2022-09-07"  # This is optional, defaults to today
 # ra = 242.58
 # dec = 11.61
 # Now no ra and dec values are given, but alertsource is set to 'icecube'. This enables GCN archive parsing for the alert name. If it is not found, it will use the latest GCN notice (these are automated).
 
-plan = PlanObservation(name=name, date=None, alertsource="icecube")
+plan = PlanObservation(
+    name=name,
+    date=None,
+    alertsource="icecube",
+    switch_filters=False,
+)
 plan.plot_target()  # Plots the observing conditions
 plan.request_ztf_fields()  # Checks in which ZTF fields this object is observable
 # plan.plot_finding_chart()
 plt.close()
 
-
-observationplan = MultiDayObservation(name=name, startdate="2022-06-23")
+observationplan = MultiDayObservation(
+    name=name, startdate="2022-09-07", switch_filters=False
+)
 observationplan.print_plan()
 summary = observationplan.summarytext
 
