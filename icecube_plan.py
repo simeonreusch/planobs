fefbb452ec2402af6af2f0f827c04e086e23f9a1
simeonreusch <simeon.reusch@desy.de>
2023-07-13 14:20:24 +0200

    only submit triggers > now

--- icecube_plan.py
+++ icecube_plan.py
@@ -2,7 +2,7 @@
 import logging
 
 import matplotlib.pyplot as plt  # type: ignore
-
+from astropy.time import Time  # type:ignore
 from planobs.api import Queue
 from planobs.models import TooTarget
 from planobs.multiday_plan import MultiDayObservation
@@ -14,7 +14,7 @@ logging.getLogger("planobs.gcn_parser").setLevel(logging.INFO)
 
 name = "IC230707A"  # Name of the alert object
 max_airmass = 1.9
-date = "2023-07-09"
+date = "2023-07-08"
 
 plan = PlanObservation(
     name=name,
@@ -28,33 +28,35 @@ plan.request_ztf_fields()  # Checks in which ZTF fields
 
 observationplan = MultiDayObservation(
     name=name,
-    startdate=None,
+    # date=date,
+    startdate=date,
     max_airmass=max_airmass,
     switch_filters=False,
-    # fieldid=434,  # if you want to override recommended field
+    fieldid=434,  # if you want to override recommended field
 )
 observationplan.print_plan()
 summary = observationplan.summarytext
-print(summary)
+
 
 triggers = observationplan.triggers
-print(triggers)
 
 q = Queue(user="DESY")
 
 for i, trigger in enumerate(triggers):
-    q.add_trigger_to_queue(
-        trigger_name=f"ToO_{name}",
-        validity_window_start_mjd=trigger["mjd_start"],
-        validity_window_end_mjd=trigger["mjd_end"],
-        targets=[
-            TooTarget(
-                field_id=trigger["field_id"],
-                filter_id=trigger["filter_id"],
-                exposure_time=trigger["exposure_time"],
-            )
-        ],
-    )
+    mjd_now = Time.now().mjd
+    if trigger["mjd_start"] > mjd_now:
+        q.add_trigger_to_queue(
+            trigger_name=f"ToO_{name}",
+            validity_window_start_mjd=trigger["mjd_start"],
+            validity_window_end_mjd=trigger["mjd_end"],
+            targets=[
+                TooTarget(
+                    field_id=trigger["field_id"],
+                    filter_id=trigger["filter_id"],
+                    exposure_time=trigger["exposure_time"],
+                )
+            ],
+        )
 
 q.print()
 # q.submit_queue()  # uncomment to submit for real
