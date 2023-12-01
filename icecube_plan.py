def9c7b1e9c5df9de1d2ddf098c4d78beb768a91
simeonreusch <simeon.reusch@desy.de>
2023-12-01 11:50:54 +0100

    trigger CI

--- icecube_plan.py
+++ icecube_plan.py
@@ -3,6 +3,7 @@ import logging
 
 import matplotlib.pyplot as plt  # type: ignore
 from astropy.time import Time  # type:ignore
+
 from planobs.api import Queue
 from planobs.models import TooTarget
 from planobs.multiday_plan import MultiDayObservation
@@ -12,13 +13,15 @@ logging.basicConfig()
 logging.getLogger("planobs.plan").setLevel(logging.INFO)
 logging.getLogger("planobs.gcn_parser").setLevel(logging.INFO)
 
-name = "IC230823A"  # Name of the alert object
-max_airmass = 1.9
+name = "IC231103A"  # Name of the alert object
+max_airmass = 2
+date = "2023-11-04"
+fieldid = 747
 # date = "2023-08-23"
 
 plan = PlanObservation(
     name=name,
-    # date=date,
+    date=date,
     alertsource="icecube",
     switch_filters=False,
     max_airmass=max_airmass,
@@ -26,16 +29,16 @@ plan = PlanObservation(
 )
 plan.plot_target()  # Plots the observing conditions
 plan.request_ztf_fields()  # Checks in which ZTF fields
-quit()
+
 
 observationplan = MultiDayObservation(
     name=name,
-    # date=date,
-    # startdate=date,
+    date=date,
+    startdate=date,
     obswindow=8,
     max_airmass=max_airmass,
     switch_filters=False,
-    # fieldid=434,  # if you want to override recommended field
+    fieldid=fieldid,  # if you want to override recommended field
 )
 observationplan.print_plan()
 summary = observationplan.summarytext
@@ -48,7 +51,7 @@ for i, trigger in enumerate(triggers):
     mjd_now = Time.now().mjd
     if trigger["mjd_start"] > mjd_now:
         q.add_trigger_to_queue(
-            trigger_name=f"ToO_{name}",
+            trigger_name=f"ToO_{name}_{fieldid}",
             validity_window_start_mjd=trigger["mjd_start"],
             validity_window_end_mjd=trigger["mjd_end"],
             targets=[
@@ -61,4 +64,4 @@ for i, trigger in enumerate(triggers):
         )
 
 q.print()
-# q.submit_queue()  # uncomment to submit for real
+q.submit_queue()  # uncomment to submit for real
